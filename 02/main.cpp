59ea32361561fc255efc104d902c3b3c351f3f52
csboo <csongiboo@gmail.com>
2024-12-06 16:20:45 +0100

    working on day2, not good yet :(

--- 02/main.cpp
+++ 02/main.cpp
@@ -8,7 +8,7 @@
 #include <sstream>
 #include <vector>
 
-static inline bool is_safe(const std::string& line);
+static bool is_safe(std::vector<int>& v, const bool& _is_already_dampened = false);
 
 int main(int argc, char* argv[]) {
   std::ifstream f = from_cli(argc, argv);
@@ -16,36 +16,51 @@ int main(int argc, char* argv[]) {
   std::string line;
   int count = 0;
   while (std::getline(f, line)) {
+    int num;
+    std::istringstream iss(line);
+    std::vector<int> line; // Probably not the most efficient, but it works well, still TODO
+      while (iss >> num) {
+      line.push_back(num);
+    }
+
     if (is_safe(line)) count++;
   }
   f.close();
-
+  
+  // NOTE answer here
   std::cout << count << "\n";
 } // main
 
-static inline bool is_safe(const std::string& line) {
-  std::istringstream iss(line);
+static bool is_safe(std::vector<int>& v, const bool& _is_already_dampened) {
+  bool _line_validity = true;
   
-  int num;
-  std::vector<int> v;
-    while (iss >> num) {
-    v.push_back(num);
-  }
   if (v.at(0) > v.at(1)) { // if its strictly decreasing
     for (int i = 1; i < v.size(); i++) {
       if (!(v.at(i - 1) - 1 >= v.at(i) && v.at(i - 1) - 3 <= v.at(i))) {
-        return false;
+        if (!_is_already_dampened) {
+          v.erase(v.begin() + i);
+          is_safe(v, true);
+        }
+        _line_validity = false;
       }
     }
   } else if (v.at(0) < v.at(1)) { // if its strictly increasing
     for (int i = 1; i < v.size(); i++) {
       if (!(v.at(i - 1) + 1 <= v.at(i) && v.at(i - 1) + 3 >= v.at(i))) {
-        return false;
+        if (!_is_already_dampened) {
+          v.erase(v.begin() + i);
+          is_safe(v, true);
+        }
+        _line_validity = false;
       }
     }
   } else { // if its equal, its not strictly monotonic (and therefore its non-safe)
-    return false;
+    if (!_is_already_dampened) {
+      v.erase(v.begin() + 1);
+      is_safe(v, true);
+    }
+    _line_validity = false;
   }
-
-  return true;
+  
+  return _line_validity;
 }

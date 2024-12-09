47b8656665402340ce9683ddf832a175e20dd684
csboo <csongiboo@gmail.com>
2024-12-09 17:05:48 +0100

    fix: fixed many things, now prints nice debug aswell!

--- 02/main.cpp
+++ 02/main.cpp
@@ -32,58 +32,74 @@ int main(int argc, char* argv[]) {
 } // main
 
 static bool is_safe(std::vector<int>& v, const bool& _is_already_dampened) {
-  std::cerr << "On report: ";
+  std::cerr << "On report: { ";
   for (const auto& i: v) {
     std::cerr << i << " ";
   }
-  std::cerr << "\n";
+  std::cerr << "}\n";
+  if (v.size() == 1) {
+    std::cerr << "report was safe\n\n";
+    return true;
+  }
   if (v.at(0) > v.at(1)) { // if its strictly decreasing
     for (int i = 1; i < v.size(); i++) {
-      std::cout << "testfor1\n";
       if (!(v.at(i - 1) - 1 >= v.at(i) && v.at(i - 1) - 3 <= v.at(i))) {
-        std::cout << "testif\n";
-        if ((v.at(i + 1) < v.at(i - 1)) && !_is_already_dampened) {
-          std::cout << "deleting: " << *v.begin() + i - 1 << "\n";
-          v.erase(v.begin() + (i - 1) );
+        std::cerr << "Non-safe level: (vector.at(" << i << ")) -> " << v.at(i) << "\n";
+        if ( (i < v.size() - 1 && (v.at(i + 1) < v.at(i - 1)) || i == v.size() - 1 ) && !_is_already_dampened) {
+          std::cerr << "Not yet dampened, deleting: " << *(v.begin() + i) << "\n";
+          v.erase(v.begin() + i);
+          std::cerr << "dampened report: { ";
+          for (const auto& i: v ) {
+            std::cerr << i << " ";
+          }
+          std::cerr << "}\n\n";
           return is_safe(v, true);
         } 
-      }
-      return false;
+        _is_already_dampened? std::cerr << "Already dampened, but still " : std::cerr << "It isn't even worth dampening, ";
+
+        std::cerr << "report was non-safe\n\n";
+        return false;
+      } 
     }
   } else if (v.at(0) < v.at(1)) { // if its strictly increasing
     for (int i = 1; i < v.size(); i++) {
       if (!(v.at(i - 1) + 1 <= v.at(i) && v.at(i - 1) + 3 >= v.at(i))) {
-        std::cerr << "Non-safe level: (vector.at(" << i + 1 << ")) -> " << i << "\n";
-        if (i < v.size() && (v.at(i + 1) > v.at(i - 1) ) && !_is_already_dampened) {
-          std::cerr << "Not yet dampened, deleting: " << *(v.begin() + (i - 1)) << "\n";
-          v.erase(v.begin() + (i - 1) );
-          std::cerr << "dampened report: ";
+        std::cerr << "Non-safe level: (vector.at(" << i << ")) -> " << v.at(i) << "\n";
+        if ( (i < v.size() - 1 && (v.at(i + 1) > v.at(i - 1)) || i == v.size() - 1 ) && !_is_already_dampened) {
+          std::cerr << "Not yet dampened, deleting: " << *(v.begin() + i) << "\n";
+          v.erase(v.begin() + i);
+          std::cerr << "dampened report: { ";
           for (const auto& i: v ) {
             std::cerr << i << " ";
           }
-          std::cerr << "\n\n";
+          std::cerr << "}\n\n";
           return is_safe(v, true);
         } 
-        std::cerr << "\n\n";
+        _is_already_dampened? std::cerr << "Already dampened, but still " : std::cerr << "It isn't even worth dampening, ";
+
+        std::cerr << "report was non-safe\n\n";
         return false;
       }
     }
-    std::cerr << "\n\n";
+    std::cerr << "report was safe\n\n";
     return true;
   } else { // is must be equal, its not strictly monotonic (and therefore its non-safe)
-    std::cerr << "Non-safe level: (vector.at(" << 1 << ")) -> " << 1 << "\n";
-    if (2 < v.size() && !_is_already_dampened) {
+    std::cerr << "Non-safe level: (vector.at(" << 1 << ")) -> " << v.at(1) << "\n";
+    if (!_is_already_dampened) {
       std::cerr << "Not yet dampened, deleting: " << *(v.begin() + 1) << "\n";
-      v.erase(v.begin());
-      std::cerr << "dampened report: ";
+      v.erase(v.begin() + 1);
+      std::cerr << "dampened report: { ";
       for (const auto& i: v ) {
         std::cerr << i << " ";
       }
-      std::cerr << "\n\n";
+      std::cerr << "}\n\n";
       return is_safe(v, true);
     } 
-    std::cerr << "\n\n";
+    _is_already_dampened? std::cerr << "Already dampened, but still " : std::cerr << "It isn't even worth dampening, ";
+
+    std::cerr << "report was non-safe\n\n";
     return false;
   }
-  return false;
+  std::cerr << "report was safe\n\n";
+  return true;
 }

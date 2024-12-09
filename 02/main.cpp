a82e2926da9daa36510b2e25e39d44f05464b894
csboo <csongiboo@gmail.com>
2024-12-09 13:42:40 +0100

    working on 2, works now ONLY for increasing

--- 02/main.cpp
+++ 02/main.cpp
@@ -32,35 +32,58 @@ int main(int argc, char* argv[]) {
 } // main
 
 static bool is_safe(std::vector<int>& v, const bool& _is_already_dampened) {
-  bool _line_validity = true;
-  
+  std::cerr << "On report: ";
+  for (const auto& i: v) {
+    std::cerr << i << " ";
+  }
+  std::cerr << "\n";
   if (v.at(0) > v.at(1)) { // if its strictly decreasing
     for (int i = 1; i < v.size(); i++) {
+      std::cout << "testfor1\n";
       if (!(v.at(i - 1) - 1 >= v.at(i) && v.at(i - 1) - 3 <= v.at(i))) {
-        if (!_is_already_dampened) {
-          v.erase(v.begin() + i);
-          is_safe(v, true);
-        }
-        _line_validity = false;
+        std::cout << "testif\n";
+        if ((v.at(i + 1) < v.at(i - 1)) && !_is_already_dampened) {
+          std::cout << "deleting: " << *v.begin() + i - 1 << "\n";
+          v.erase(v.begin() + (i - 1) );
+          return is_safe(v, true);
+        } 
       }
+      return false;
     }
   } else if (v.at(0) < v.at(1)) { // if its strictly increasing
     for (int i = 1; i < v.size(); i++) {
       if (!(v.at(i - 1) + 1 <= v.at(i) && v.at(i - 1) + 3 >= v.at(i))) {
-        if (!_is_already_dampened) {
-          v.erase(v.begin() + i);
-          is_safe(v, true);
-        }
-        _line_validity = false;
+        std::cerr << "Non-safe level: (vector.at(" << i + 1 << ")) -> " << i << "\n";
+        if (i < v.size() && (v.at(i + 1) > v.at(i - 1) ) && !_is_already_dampened) {
+          std::cerr << "Not yet dampened, deleting: " << *(v.begin() + (i - 1)) << "\n";
+          v.erase(v.begin() + (i - 1) );
+          std::cerr << "dampened report: ";
+          for (const auto& i: v ) {
+            std::cerr << i << " ";
+          }
+          std::cerr << "\n\n";
+          return is_safe(v, true);
+        } 
+        std::cerr << "\n\n";
+        return false;
       }
     }
-  } else { // if its equal, its not strictly monotonic (and therefore its non-safe)
-    if (!_is_already_dampened) {
-      v.erase(v.begin() + 1);
-      is_safe(v, true);
-    }
-    _line_validity = false;
+    std::cerr << "\n\n";
+    return true;
+  } else { // is must be equal, its not strictly monotonic (and therefore its non-safe)
+    std::cerr << "Non-safe level: (vector.at(" << 1 << ")) -> " << 1 << "\n";
+    if (2 < v.size() && !_is_already_dampened) {
+      std::cerr << "Not yet dampened, deleting: " << *(v.begin() + 1) << "\n";
+      v.erase(v.begin());
+      std::cerr << "dampened report: ";
+      for (const auto& i: v ) {
+        std::cerr << i << " ";
+      }
+      std::cerr << "\n\n";
+      return is_safe(v, true);
+    } 
+    std::cerr << "\n\n";
+    return false;
   }
-  
-  return _line_validity;
+  return false;
 }

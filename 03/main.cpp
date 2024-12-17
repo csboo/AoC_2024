930a0c2d2e83afd6f3d2b6c31f255db5ed1846de
csboo <csongiboo@gmail.com>
2024-12-17 12:35:28 +0100

    regex still don't work

--- 03/main.cpp
+++ 03/main.cpp
@@ -3,11 +3,8 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
-
-char next();
-
-bool check(char& c, const int& level);
 
 int main(int argc, char* argv[]) {
   std::ifstream f = from_cli(argc, argv);
@@ -17,7 +14,20 @@ int main(int argc, char* argv[]) {
     data.append(temp);
   }
   // std::cerr << data;
-  
+
+  std::regex regex("mul\\(\\d{1,3},\\d{1,3}\\)");
+  auto current_match = std::sregex_iterator(data.begin(), data.end(), regex);
+  auto data_end = std::sregex_iterator();
+ 
+    
+  while (current_match != data_end) {
+
+      std::smatch match = *current_match;
+      int num1 = std::stoi(match[1].str());
+      int num2 = std::stoi(match[2].str());
+      std::cout << "First number: " << num1 << std::endl;
+      std::cout << "Second number: " << num2 << std::endl;
+      ++current_match;
+  }
   f.close();
-  
 }
